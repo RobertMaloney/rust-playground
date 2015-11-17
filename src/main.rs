d413f510ae19fdc7d52bb0420b079de1e9150d8f
Robert Maloney <robert.maloney@mycase.com>
2015-11-17 14:45:10 -0800

    rm: partially implement grab_words method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn grab_words(count: usize, len: usize) -> Vec<String> {
+  let mut v = vec![];
+  let file_name = "words.txt";
+  let f = File::open(file_name).unwrap();
+  let mut br = BufReader::new(f);
+  while v.len() < count {
+    let mut s = String::new();
+    br.read_line(&mut s).unwrap();
+    s.pop(); // remove the newline from the end
+    v.push(s);
+  }
+  v
+}
+
 fn main() {
-    println!("Hello, world!");
+  let my_words: Vec<String> = grab_words(3,0);
+  for word in my_words {
+    println!("{}", word);
+  }
 }
