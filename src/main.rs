39f27d56149b1a6b2c7f5070bba5bf26e44bcc83
Robert Maloney <robert.maloney@mycase.com>
2015-11-18 10:43:27 -0800

    rm: add parameter count, fix off-by-one

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use rand::{thread_rng, Rng};
 
-fn grab_words(len: usize) -> Vec<String> {
+fn grab_words(len: usize, count: usize) -> Vec<String> {
   let mut rng = thread_rng();
   let file_name = "words.txt";
   let f = File::open(file_name).unwrap();
   let br = BufReader::new(f);
-  let words: Vec<_> = br.lines().filter(|x| x.as_ref().unwrap().len() == len).map(|x| x.unwrap()).collect();
+  let words: Vec<_> = br.lines().filter(|x| x.as_ref().unwrap().len() == len + 1).map(|x| x.unwrap()).collect();
   let domain_size = words.len();
   let mut v: Vec<String> = vec![];
-  for _ in 0..15 {
+  for _ in 0..count {
     let index = (rng.next_u32() as usize) % domain_size;
     let s = words.get(index).unwrap();
     v.push(s.clone());
@@ -22,7 +22,7 @@ fn grab_words(len: usize) -> Vec<String> {
 }
 
 fn main() {
-  let my_words: Vec<String> = grab_words(5);
+  let my_words: Vec<String> = grab_words(5, 20);
   for word in my_words {
     println!("{}", word);
   }
