1880d70488f433c88447b044068a102d11a354d4
Robert Maloney <robert.maloney@mycase.com>
2015-11-19 10:40:20 -0800

    rm: handle command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+  let example = "X -/ X 5- 8/ 9- X 81 1- 4/X";
+  let args: Vec<_> = env::args().collect();
+  if args.len() != 2 {
+    println!("Wong number of arguments.");
+    return;
+  }
 }
